47d636979ee8caf1dba474987a9ff17034946ee3
apyshonkin <apyshonkin@aventica.ru>
2024-07-02 07:14:21 +0300

    all nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,6 @@ provider "proxmox" {
   endpoint = var.pm_api_url
   username = var.pm_user
   password = var.pm_password
-  # because self-signed TLS certificate is in use
   insecure = true
   tmp_dir  = "/var/tmp"
 
@@ -31,16 +30,23 @@ data "local_file" "ssh_public_key" {
   filename = "/Users/lain/.ssh/id_ed25519.pub"
 }
 
-resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
-  name      = "test-ubuntu"
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "pve"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
+
+resource "proxmox_virtual_environment_vm" "kube-master-0" {
+  name      = "kube-master-0"
   node_name = "pve"
 
   cpu {
-    cores   = "8"
+    cores   = "1"
   }
 
   memory {
-    dedicated = "8192"
+    dedicated = "2048"
   }
 
   network_device {
@@ -82,15 +88,110 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   }
 }
 
-resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+resource "proxmox_virtual_environment_vm" "kube-master-1" {
+  name      = "kube-master-1"
+  node_name = "pve"
+
+  cpu {
+    cores   = "1"
+  }
+
+  memory {
+    dedicated = "2048"
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  tpm_state {
+    version = "v2.0"
+  }
+
+  serial_device {}
+
+  initialization {
+
+    ip_config {
+      ipv4 {
+        address = "192.168.1.202/24"
+        gateway = "192.168.1.1"
+      }
+    }
+
+    user_account {
+      username = "ubuntu"
+      keys     = [trimspace(data.local_file.ssh_public_key.content)]
+    }
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+  }
 }
 
-resource "proxmox_virtual_environment_vm" "centos_vm" {
-  name      = "test-centos"
+resource "proxmox_virtual_environment_vm" "kube-master-2" {
+  name      = "kube-master-2"
+  node_name = "pve"
+
+  cpu {
+    cores   = "1"
+  }
+
+  memory {
+    dedicated = "2048"
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  tpm_state {
+    version = "v2.0"
+  }
+
+  serial_device {}
+
+  initialization {
+
+    ip_config {
+      ipv4 {
+        address = "192.168.1.203/24"
+        gateway = "192.168.1.1"
+      }
+    }
+
+    user_account {
+      username = "ubuntu"
+      keys     = [trimspace(data.local_file.ssh_public_key.content)]
+    }
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "kube-node-0" {
+  name      = "kube-node-0"
   node_name = "pve"
 
   cpu {
@@ -116,21 +217,23 @@ resource "proxmox_virtual_environment_vm" "centos_vm" {
   serial_device {}
 
   initialization {
+
     ip_config {
       ipv4 {
-        address = "192.168.1.202/24"
+        address = "192.168.1.204/24"
         gateway = "192.168.1.1"
       }
     }
+
     user_account {
-      username = "centos"
+      username = "ubuntu"
       keys     = [trimspace(data.local_file.ssh_public_key.content)]
     }
   }
 
   disk {
     datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_download_file.centos_cloud_image.id
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
     interface    = "virtio0"
     iothread     = true
     discard      = "on"
@@ -138,10 +241,104 @@ resource "proxmox_virtual_environment_vm" "centos_vm" {
   }
 }
 
-resource "proxmox_virtual_environment_download_file" "centos_cloud_image" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = "pve"
-  url          = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-1508.qcow2"
-  file_name    = "centos7.img"
+resource "proxmox_virtual_environment_vm" "kube-node-1" {
+  name      = "kube-node-1"
+  node_name = "pve"
+
+  cpu {
+    cores   = "8"
+  }
+
+  memory {
+    dedicated = "8192"
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  tpm_state {
+    version = "v2.0"
+  }
+
+  serial_device {}
+
+  initialization {
+
+    ip_config {
+      ipv4 {
+        address = "192.168.1.205/24"
+        gateway = "192.168.1.1"
+      }
+    }
+
+    user_account {
+      username = "ubuntu"
+      keys     = [trimspace(data.local_file.ssh_public_key.content)]
+    }
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "kube-storage-0" {
+  name      = "kube-storage-0"
+  node_name = "pve"
+
+  cpu {
+    cores   = "1"
+  }
+
+  memory {
+    dedicated = "2048"
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  tpm_state {
+    version = "v2.0"
+  }
+
+  serial_device {}
+
+  initialization {
+
+    ip_config {
+      ipv4 {
+        address = "192.168.1.206/24"
+        gateway = "192.168.1.1"
+      }
+    }
+
+    user_account {
+      username = "ubuntu"
+      keys     = [trimspace(data.local_file.ssh_public_key.content)]
+    }
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+  }
 }
